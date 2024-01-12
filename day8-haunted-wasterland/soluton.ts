cc65cc6b3c5893980f664777d75af6f02e7328d8
Julia Lepner <ylepner@gmail.com>
2024-01-12 13:39:51 +0100

    fix: correct solution

--- day8-haunted-wasterland/soluton.ts
+++ day8-haunted-wasterland/soluton.ts
@@ -1,3 +1,4 @@
+import { count } from "console";
 import { readFile } from "fs/promises";
 import path from "path";
 
@@ -16,13 +17,12 @@ interface NodeData {
 async function solve() {
   const file = path.join(__dirname, './data.txt');
   const data = await readFile(file, { encoding: 'utf-8' });
-  steps = data.split('\n')[0].split('');
+  steps = data.trim().split('\n')[0].trim().split('');
   const instructions = data.split('\n').slice(2);
   const dataRes = splitData(instructions);
-  const finishNode = dataRes[dataRes.length - 1].node.trim();
   const tree = createTree(dataRes);
   if (tree) {
-    console.log(countSteps(tree, finishNode, steps));
+    console.log(countSteps(tree, 'ZZZ', steps));
   }
 }
 
@@ -31,7 +31,7 @@ solve();
 function splitData(data: string[]) {
   const res = data.map(el => el.split('='));
   const instructions = res.map(el => {
-    return { node: el[0], elements: el[1].replace('(', '').replace(')\r', '').split(', ').map(el => el.trim()) }
+    return { node: el[0].trim(), elements: el[1].trim().replace('(', '').replace(')', '').split(', ').map(el => el.trim()) }
   })
   return instructions;
 }
@@ -42,40 +42,47 @@ function createTree(dataStructure: NodeData[]) {
     return null
   }
   dataStructure.forEach(el => {
-    const newNode: Node = { id: el.node.trim() };
-    nodeMap[el.node.trim()] = newNode;
+    const newNode: Node = { id: el.node };
+    nodeMap[el.node] = newNode;
   })
 
   dataStructure.forEach(el => {
-    const currentNode = nodeMap[el.node.trim()];
-    const [leftElement, rightElement] = el.elements.map(el => el.trim());
+    const currentNode = nodeMap[el.node];
+    const [leftElement, rightElement] = el.elements;
     if (leftElement) {
+      if (currentNode.left) {
+        throw new Error(`Element already there! ${currentNode.id} ${currentNode.left}`)
+      }
       currentNode.left = nodeMap[leftElement];
+    } else {
+      throw new Error('Wrong data left')
     }
     if (rightElement) {
+      if (currentNode.right) {
+        throw new Error(`Element already there! ${currentNode.id} ${currentNode.right}`)
+      }
       currentNode.right = nodeMap[rightElement];
+    } else {
+      throw new Error('Wrong data left')
     }
   })
-  return nodeMap[dataStructure[0].node.trim()]
+  return nodeMap['AAA']
 }
 
-function countSteps(root: Node, targetEl: string, steps: string[]) {
+function countSteps(root: Node, target: string, steps: string[]) {
   let currentNode = root;
-  let stepsCounter = 0;
-  steps.pop();
+  let totalSteps = 0;
   while (currentNode.id !== 'ZZZ') {
-    let counter = 0;
-    steps.forEach(step => {
-      if (step === 'L' && currentNode.left) {
-        currentNode = currentNode.left;
-      } else if (step === 'R' && currentNode.right) {
-        currentNode = currentNode.right;
-      } else {
-        currentNode = currentNode;
-      }
-      counter++;
-      stepsCounter++;
-    })
+    let countNumber = totalSteps % steps.length;
+    if (steps[countNumber] === 'L' && currentNode.left) {
+      currentNode = currentNode.left;
+    } else if (steps[countNumber] === 'R' && currentNode.right) {
+      currentNode = currentNode.right;
+    } else {
+      throw new Error(`${currentNode}, ${totalSteps}`)
+    }
+    console.log(currentNode.id);
+    totalSteps++;
   }
-  return stepsCounter;
+  return totalSteps;
 }
