db36a8a0903035f703115c79adc45422c3261aa9
Julia Lepner <ylepner@gmail.com>
2024-01-30 14:04:24 +0100

    feat: complete task day 14

--- day14-parabolic-reflector-dish/solution.ts
+++ day14-parabolic-reflector-dish/solution.ts
@@ -1,15 +1,50 @@
 export function solve14(data: string) {
-  console.log(data);
-  const split = data.trim().split('\n').map(el => el.trim().split(''));
-  const result = revertGrid(split);
+  const split = data.trim().split('\n').map(el => el.trim());
+  const revertedArr = revertGrid(split);
+  const result = revertedArr.map(el => transformSingleString(el.join('')));
+  const total = countTotal(result);
+  return total;
+}
+
+export function transformSingleString(str: string) {
+  let result = '';
+  let roundedCount = 0;
+  let cubeCount = 0;
+  let pointCounter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== '#') {
+      cubeCount = 0;
+      if (str[i] === 'O') {
+        roundedCount += 1;
+        continue;
+      } else {
+        pointCounter += 1;
+      }
+    } else {
+      if (roundedCount === 0 && pointCounter === 0) {
+        result += '#'.repeat(1)
+      } else {
+        result += 'O'.repeat(roundedCount);
+        result += '.'.repeat(pointCounter);
+        result += '#'.repeat(1);
+        roundedCount = 0;
+        pointCounter = 0;
+      }
+    }
+  }
+  if (result.length < str.length) {
+    result += 'O'.repeat(roundedCount);
+    result += '.'.repeat(pointCounter);
+  }
   return result;
 }
 
-function revertGrid(grid: string[][]): string[][] {
+function revertGrid(grid: string[]): string[][] {
   let array = [];
   let subarray: string[] = [];
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
+      const el = grid[j][i];
       subarray.push(grid[j][i])
     }
     array.push(subarray);
@@ -19,9 +54,10 @@ function revertGrid(grid: string[][]): string[][] {
 }
 
 function countTotal(array: string[]) {
-  const results = array.map((el, i) => {
+  const revertedArr = revertGrid(array);
+  const results = revertedArr.map((el, i) => {
     const amount = Array.from(el).filter(symbol => symbol === 'O')
-    return amount.length * (10 - i)
+    return amount.length * (array.length - i)
   })
   return results.reduce((a, b) => a + b);
 }
--- day14-parabolic-reflector-dish/test.spec.ts
+++ day14-parabolic-reflector-dish/test.spec.ts
@@ -1,24 +1,63 @@
-import { solve14 } from "./solution"
+import { readFile } from "fs/promises"
+import path from "path"
+import { solve14, transformSingleString } from "./solution"
 
 const test1 = `
-OOOO.#.O..
-OO..#....#
-OO..O##..O
-O..#.OO...
-........#.
-..#....#.#
-..O..#.O.O
-..O.......
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
 #....###..
-#....#....`
+#OO..#....
+`
 
 const test2 = `
 OO.O.O..##
 `
 
-describe('Single test', () => {
-  // test('tint', () => {
-  //   const result = solve14(test2.trim());
-  //   expect(result).toBe('OOOO...##');
-  // })
+const test3 = `
+.#O..#O...`
+
+const test4 = `
+OO..#....#
+`
+
+describe('Single string test', () => {
+  test('tint', () => {
+    const result = transformSingleString(test2.trim());
+    expect(result).toBe('OOOO....##');
+  })
+  test('tint', () => {
+    const result = transformSingleString(test3.trim());
+    expect(result).toBe('.#O..#O...');
+  })
+  test('tint', () => {
+    const result = transformSingleString(test4.trim());
+    expect(result).toBe('OO..#....#');
+  })
 })
+
+describe('Test an array of strings', () => {
+  test('Should revert and tint', () => {
+    const result = solve14(test1.trim());
+    expect(result).toBe(136);
+  })
+})
+
+describe('Find result on big data', () => {
+  test('Should receive result', async () => {
+    const data = await getData();
+    const result = solve14(data);
+    console.log(result);
+  })
+})
+
+async function getData() {
+  const file = path.join(__dirname, './data.txt');
+  const result = await readFile(file, { encoding: 'utf-8' });
+  return result;
+}
