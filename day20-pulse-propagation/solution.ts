4291fd2c7ace3c3d0b546ea5188ba98ca16ce9a6
Julia Lepner <ylepner@gmail.com>
2024-03-14 14:43:35 +0100

    feat: complete solution day 20

--- day20-pulse-propagation/solution.ts
+++ day20-pulse-propagation/solution.ts
@@ -1,5 +1,5 @@
 interface ModuleMemory {
-  [key: string]: string | { [key: string]: string };
+  [key: string]: string;
 }
 
 interface Module {
@@ -12,20 +12,20 @@ interface Module {
 const modules: { [key: string]: Module } = {};
 let broadcastTargets: string[] = [];
 
-export function solveTest(input: string) {
+export function solve(input: string) {
   const lines = input.trim().split('\n').map(line => line.trim());
   let memory;
   for (const line of lines) {
-    const [left, right] = line.trim().split(" -> ");
-    const outputs = right.split(", ");
+    const [left, right] = line.trim().split(' -> ');
+    const outputs = right.split(', ');
 
-    if (left === "broadcaster") {
+    if (left === 'broadcaster') {
       broadcastTargets = outputs;
     } else {
       const type = left[0];
       const name = left.slice(1);
-      if (type === "%") {
-        memory = "off";
+      if (type === '%') {
+        memory = 'off';
       } else {
         memory = {};
       }
@@ -37,56 +37,57 @@ export function solveTest(input: string) {
       };
     }
   }
-  const modulesArr = Object.entries(modules);
-  for (const [name, module] of Object.entries(modules)) {
-    for (const output of module.outputs) {
+
+  const modulesArray: [string, Module][] = Object.entries(modules);
+  for (const [num, module] of Object.entries(modulesArray)) {
+    for (const output of module[1].outputs) {
       if (modules.hasOwnProperty(output)) {
-        const test = modules[output].type;
-        if (modules[output].type === "&") {
-          (modules[output].memory as ModuleMemory)[name] = "lo";
+        if (modules[output].type === '&') {
+          (modules[output].memory as ModuleMemory)[module[0]] = 'low';
         }
       }
     }
+  }
 
-    let lo = 0;
-    let hi = 0;
+  let low = 0;
+  let high = 0;
 
-    for (let _ = 0; _ < 1; _++) {
-      lo++;
-      const q: [string, string, string][] = broadcastTargets.map(target => ["broadcaster", target, "lo"]);
+  for (let i = 0; i < 1000; i++) {
+    low++;
+    const q: [string, string, string][] = broadcastTargets.map(target => ['broadcaster', target, 'low']);
 
-      while (q.length > 0) {
-        const [origin, target, pulse] = q.shift()!;
+    while (q.length > 0) {
+      const [origin, target, pulse] = q.shift()!;
 
-        if (pulse === "lo") {
-          lo++;
-        } else {
-          hi++;
-        }
+      if (pulse === 'low') {
+        low++;
+      } else {
+        high++;
+      }
 
-        if (!(target in modules)) {
-          continue;
-        }
+      if (!(target in modules)) {
+        continue;
+      }
 
-        const module = modules[target];
+      const module = modules[target];
 
-        if (module.type === "%") {
-          if (pulse === "lo") {
-            module.memory = (module.memory === "off") ? "on" : "off";
-            const outgoing = (module.memory === "on") ? "hi" : "lo";
-            for (const x of module.outputs) {
-              q.push([module.name, x, outgoing]);
-            }
-          }
-        } else {
-          (module.memory as ModuleMemory)[origin] = pulse;
-          const outgoing = Object.values(module.memory as ModuleMemory).every(x => x === "hi") ? "lo" : "hi";
+      if (module.type === '%') {
+        if (pulse === 'low') {
+          module.memory = (module.memory === 'off') ? 'on' : 'off';
+          const outgoing = (module.memory === 'on') ? 'high' : 'low';
           for (const x of module.outputs) {
             q.push([module.name, x, outgoing]);
           }
         }
+      } else {
+        (module.memory as ModuleMemory)[origin] = pulse;
+        const outgoing = Object.values(module.memory as ModuleMemory).every(x => x === 'high') ? 'low' : 'high';
+        outgoing;
+        for (const x of module.outputs) {
+          q.push([module.name, x, outgoing]);
+        }
       }
     }
-    return lo * hi;
   }
+  return low * high;
 }
--- day20-pulse-propagation/test.spec.ts
+++ day20-pulse-propagation/test.spec.ts
@@ -1,5 +1,4 @@
-import { convertData } from "./solution2";
-import { solveTest } from "./solution";
+import { solve } from "./solution";
 import { readFile } from "fs/promises";
 import path from "path";
 
@@ -21,25 +20,20 @@ broadcaster -> a
 
 
 describe('Day 20, https://adventofcode.com/2023/day/20', () => {
-  test('Should convert data to Connections Test 1', () => {
-    const result = convertData(test1);
-    expect(result).toBeDefined();
-  })
-  test('Should convert data to Connections Test 2', () => {
-    const result = convertData(test2);
-    expect(result).toBeDefined();
-  })
-  test('Test 1', () => {
-    const result = solveTest(test1);
+  test('Should get result test 1', () => {
+    const result = solve(test1);
     expect(result).toBe(32000000);
   })
+  // test('Should get result test 2', () => {
+  //   const result = solve(test2);
+  //   expect(result).toBe(11687500);
+  // })
   test('Test big data', async () => {
     const data = await getData();
-    const result = solveTest(data);
+    const result = solve(data);
     expect(result).toBeLessThan(910664370);
   })
 })
-
 
 async function getData() {
   const file = path.join(__dirname, 'data.txt');
